76734b1d9532590fe6b47a0d774b21c570ffa5cd
Narishma-gb <194818981+Narishma-gb@users.noreply.github.com>
2026-04-25 13:45:45 +0200

    Pic Banks 18, 21-32, partial Bank 46

--- wip.asm
+++ wip.asm
@@ -47,10 +47,6 @@ INCLUDE "engine/dumps/bank7f.asm"
 EXPORT DEF EggPic EQU $7b57
 
 
-;SECTION "rom18", ROMX[$4000], BANK[18]
-; ROM $12 : $48000 - $4BFFF
-
-
 SECTION "rom20", ROMX[$4000], BANK[20]
 ; ROM $14 : $50000 - $53FFF
 
@@ -95,54 +91,6 @@ SECTION "rom20", ROMX[$4000], BANK[20]
 	dr GetTrainerPic, $5974
 	dr DecompressGet2bpp, $59ce
 	dr BaseData, $5bdf
-
-
-;SECTION "rom21", ROMX[$4000], BANK[21]
-; ROM $15 : $54000 - $57FFF
-
-
-;SECTION "rom22", ROMX[$4000], BANK[22]
-; ROM $16 : $58000 - $5BFFF
-
-
-;SECTION "rom23", ROMX[$4000], BANK[23]
-; ROM $17 : $5C000 - $5FFFF
-
-
-;SECTION "rom24", ROMX[$4000], BANK[24]
-; ROM $18 : $60000 - $63FFF
-
-
-;SECTION "rom25", ROMX[$4000], BANK[25]
-; ROM $19 : $64000 - $67FFF
-
-
-;SECTION "rom26", ROMX[$4000], BANK[26]
-; ROM $1a : $68000 - $6BFFF
-
-
-;SECTION "rom27", ROMX[$4000], BANK[27]
-; ROM $1b : $6C000 - $6FFFF
-
-
-;SECTION "rom28", ROMX[$4000], BANK[28]
-; ROM $1c : $70000 - $73FFF
-
-
-;SECTION "rom29", ROMX[$4000], BANK[29]
-; ROM $1d : $74000 - $77FFF
-
-
-;SECTION "rom30", ROMX[$4000], BANK[30]
-; ROM $1e : $78000 - $7BFFF
-
-
-;SECTION "rom31", ROMX[$4000], BANK[31]
-; ROM $1f : $7C000 - $7FFFF
-
-
-;SECTION "rom32", ROMX[$4000], BANK[32]
-; ROM $20 : $80000 - $83FFF
 
 
 SECTION "rom33", ROMX[$4000], BANK[33]
@@ -241,7 +189,7 @@ SECTION "rom38", ROMX[$4000], BANK[38]
 ; ROM $2b : $AC000 - $AFFFF
 
 
-SECTION "rom46", ROMX[$4000], BANK[46]
+SECTION "rom46", ROMX[$6300], BANK[46]
 ; ROM $2e : $B8000 - $BBFFF
 
 	dr CheckForHiddenItems, $6300
